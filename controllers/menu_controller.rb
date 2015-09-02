50105d72ea6e854ffc730fafb8e60f2b5e5e7110
Caleb Schappa <cschappa@users.noreply.github.com>
2015-09-02 16:54:28 -0400

    assignment 22 completed

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -10,10 +10,11 @@ class MenuController
   def main_menu
     puts "Main menu - #{address_book.entries.count} entries"
     puts "1 - View all entries"
-    puts "2 - Create an entry"
-    puts "3 - Search for an entry"
-    puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "2 - View Entry Number n"
+    puts "3 - Create an entry"
+    puts "4 - Search for an entry"
+    puts "5 - Import entries from a CSV"
+    puts "6 - Exit"
     print "Enter you selection: "
 
     selection = gets.to_i
@@ -26,17 +27,21 @@ class MenuController
       main_menu
     when 2
       system "clear"
-      create_entry
+      view_entry_n
       main_menu
     when 3
       system "clear"
-      search_entries
+      create_entry
       main_menu
     when 4
       system "clear"
-      read_csv
+      search_entries
       main_menu
     when 5
+      system "clear"
+      read_csv
+      main_menu
+    when 6
       puts "Good-bye"
       exit(0)
     else
@@ -54,6 +59,43 @@ class MenuController
     end
     system "clear"
     puts "End of entries"    
+  end
+
+  def view_entry_n
+    num_entries = @address_book.entries.count
+    system "clear"
+
+    # check to make sure we have entries to display
+    if num_entries == 0
+      puts "AddressBloc is empty, Please add entries"
+      return
+    end
+    
+    # prompt user until we get a valid input
+    loop do
+
+      puts "Please enter a vaild entry number between 0 - #{num_entries-1}"
+      print "Which entry number would you like to view?: "
+      index = gets.chomp
+      
+      if index == "m"
+        # user want to return to main menu, break out of loop and return
+        system "clear"
+        break
+      end
+
+      if (0...num_entries).include?(index.to_i)
+        puts @address_book.entries[index.to_i].to_s
+        break
+      else
+        # index isn't valid, give some quidance and lets try again
+        system "clear"
+        puts "Invalid entry: #{index}"
+        puts "enter m to return to the main menu"
+      end
+
+    end
+    
   end
  
   def create_entry
